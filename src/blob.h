78e029844fd78ab61b66410aba497549be81d6f1
KunLiang <lionkun1223@163.com>
2020-02-11 16:49:36 +0800

    Class Blob has be created. Task 1 and Task 2 has been completed. Change files to avoid confilcts

---
+++ src/blob.h
@@ -0,0 +1,27 @@
+#ifndef IE_BLOB_H
+#define IE_BLOB_H
+
+#include <napi.h>
+
+#include "inference_engine.hpp"
+
+namespace ienodejs {
+
+class Blob : public Napi::ObjectWrap<Blob> {
+ public:
+  static void Init(const Napi::Env& env);
+  static Napi::Value NewInstance(const Napi::Env& env,
+                                 const InferenceEngine::Blob::Ptr& actual);
+  Blob(const Napi::CallbackInfo& info);
+
+ private:
+  static Napi::FunctionReference constructor;
+  // APIs
+  Napi::Value Buffer(const Napi::CallbackInfo& info);
+
+  InferenceEngine::Blob::Ptr actual_;
+};
+
+}  // namespace ienodejs
+
+#endif  // IE_BLOB_H
